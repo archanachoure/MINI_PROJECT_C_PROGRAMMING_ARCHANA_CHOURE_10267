52d723160cfb895fb730572162aa4b46882a4c93
archanachoure <archana.choure@rungta.org>
2026-01-01 15:24:56 +0530

    Initial commit - Loop Optimization Demo

---
+++ CompareLoops.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    // NAME - ARCHANA CHOURE 
+    //  ERP - 10267 
+//      Loop Optimization Demo
+// ----------------------
+// For Loop Iterations   : 10
+// While Loop Iterations : 10
+    int limit = 10;
+    int forCount = 0;
+    int whileCount = 0;
+    int i;   
+    
+    for (i = 1; i <= limit; i++) {
+        forCount++;
+    }
+
+    
+    i = 1;
+    while (i <= limit) {
+        whileCount++;
+        i++;
+    }
+    printf("Loop Optimization Demo\n");
+    printf("----------------------\n");
+    printf("For Loop Iterations   : %d\n", forCount);
+    printf("While Loop Iterations : %d\n", whileCount);
+
+    return 0;
+}
